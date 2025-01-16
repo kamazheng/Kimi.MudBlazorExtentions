5533cb58823e9c8e040c5c804fcb87357ceddb03
kamazheng <kai.zheng@molex.com>
2025-01-16 09:04:48 +0800

    Refactor SnackbarExtensions and add new methods
    Renamed namespace from `Kimi.MudBlazorExtentions.Dialogs` to `Kimi.MudBlazorExtentions.Snackbar`.
    Renamed class `SnackBarExtensions` to `SnackbarExtensions`.
    Renamed method `ShowException` to `Exception`.
    Added new methods: `Error`, `Info`, and `Warning` to `SnackbarExtensions`.
    Added documentation comments to each method.
    Included author and creation date information at the top of the file.

---
+++ Kimi.MudBlazorExtentions/Snackbar/SnackBarExtensions.cs
@@ -0,0 +1,59 @@
+﻿// ***********************************************************************
+// Author           : MOLEX\kzheng
+// Created          : 01/16/2025
+// ***********************************************************************
+
+using MudBlazor;
+
+namespace Kimi.MudBlazorExtentions.Snackbar
+{
+    /// <summary>
+    /// Defines the <see cref="SnackbarExtensions" />
+    /// </summary>
+    public static class SnackbarExtensions
+    {
+        #region Methods
+
+        /// <summary>
+        /// The Error
+        /// </summary>
+        /// <param name="snackBar">The snackBar<see cref="ISnackbar"/></param>
+        /// <param name="msg">The msg<see cref="string"/></param>
+        public static void Error(this ISnackbar snackBar, string msg)
+        {
+            snackBar.Add(msg, Severity.Error);
+        }
+
+        /// <summary>
+        /// The Info
+        /// </summary>
+        /// <param name="snackBar">The snackBar<see cref="ISnackbar"/></param>
+        /// <param name="msg">The msg<see cref="string"/></param>
+        public static void Info(this ISnackbar snackBar, string msg)
+        {
+            snackBar.Add(msg, Severity.Info);
+        }
+
+        /// <summary>
+        /// The ShowException
+        /// </summary>
+        /// <param name="snackBar">The snackBar<see cref="ISnackbar"/></param>
+        /// <param name="ex">The ex<see cref="Exception"/></param>
+        public static void Exception(this ISnackbar snackBar, Exception ex)
+        {
+            snackBar.Add($"{ex.Message}\n{ex.InnerException?.Message}", Severity.Error);
+        }
+
+        /// <summary>
+        /// The Warning
+        /// </summary>
+        /// <param name="snackBar">The snackBar<see cref="ISnackbar"/></param>
+        /// <param name="msg">The msg<see cref="string"/></param>
+        public static void Warning(this ISnackbar snackBar, string msg)
+        {
+            snackBar.Add(msg, Severity.Warning);
+        }
+
+        #endregion
+    }
+}
